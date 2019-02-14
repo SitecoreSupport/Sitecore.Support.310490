b7365984f978f45ff881796c89d2c157aad3bdfc
Ivan Sheyenko <ivs@sitecore.net>
2019-02-14 13:02:04 -0500

    Introduce verification action to check that posted fields collection matches form defintion

---
+++ src/Sitecore.Support.310490/ValidateFormIntegrity.cs
@@ -0,0 +1,30 @@
+﻿using Sitecore.Data;
+using Sitecore.Diagnostics;
+using Sitecore.WFFM.Abstractions.Actions;
+using Sitecore.WFFM.Actions.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support
+{
+  public class ValidateFormIntegrity : WffmCheckAction
+  {
+    public override void Execute(ID formid, IEnumerable<ControlResult> fields, ActionCallContext actionCallContext = null)
+    {
+      try
+      {
+        foreach (var field in actionCallContext.FormItem.Fields)
+        {
+          var ok = fields.First(f => ID.Parse(f.FieldID) == field.ID);
+        }
+      }
+      catch (InvalidOperationException e)
+      {
+        Log.Warn(string.Format("Posted field collection does not match the form {0} definition.",formid), e, this);
+        throw;
+      }
+    }
+  }
+}
